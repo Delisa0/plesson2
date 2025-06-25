7f04bb86715e6acbdb59018fa555c31b02353755
Delisa0 <elahidelisa0@gmail.com>
2025-06-25 20:28:30 +1000

    operators

---
+++ activity 2.py
@@ -0,0 +1,16 @@
+num1 =45
+num2 =3
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition:",num1+num2)
+print("difference:",num1-num2)
+print("product:", num1*num2)
+print("division:",num1/num2)
+print("Floor Division:", num1//num2)
+print("Moduls Operation:",num1%num2)
+print("Square:",num2**2)
+print("Square Root:",num1**0.5)
+print("equal?",num1==num2)
+print("Number 1 greater?",num1>num2)
+print("Number 2 greater?",num1<num2)
+print("Not Equal?",num1!=num2)
